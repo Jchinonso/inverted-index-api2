caba8a842df582e1ecb5235ebbce29b17d18fb31
Johnson Chinonso <johsonchinonso19@gmail.com>
2017-05-11 13:36:42 +0100

    server config setting

---
+++ app.js
@@ -0,0 +1,22 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import superObj from './tests/super-test';
+import router from './route/api';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+	console.log('server now running at ' + process.env.PORT);
+});
+
+// load the routes
+app.use('/', router);
+
+superObj.create(app);   // load supertest for create endpoint
+superObj.search(app);   // load supertest for search endpoint
